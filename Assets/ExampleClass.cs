9936cee57b76d6738846bbf6751bd6731ccf3c61
张敦勇 <524547312@qq.com>
2023-08-29 01:20:12 +0800

    1

---
+++ Assets/ExampleClass.cs
@@ -0,0 +1,22 @@
+﻿// Smooth towards the target
+
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, 0));
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        Debug.LogError(Vector3.Distance(transform.position, targetPosition) + " " + velocity + " " + Time.time);
+    }
+}
---
+++ Assets/SecondOrderDynamics.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondOrderDynamics : MonoBehaviour
+{
+    Vector3 xp;
+    Vector3 y, yd;
+    public float f, z, r;
+    public Transform target;
+
+    float k1, k2, k3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xp = target.position;
+        y = target.position;
+        yd = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        k1 = z / (Mathf.PI * f);
+        k2 = 1 / ((2 * Mathf.PI * f) * (2 * Mathf.PI * f));
+        k3 = r * z  / (2 * Mathf.PI * f);
+
+
+        var T = Time.deltaTime;
+        var x = target.position;
+        var xd = (x - xp) / T;
+        xp = x;
+
+        float K2_stable = Mathf.Max(k2, 1.1f * (T * T / 4f + T * k1 / 2f));
+        y = y + T * yd;
+        yd = yd + T * (x + k3 * xd - y - k1 * yd) / K2_stable;
+
+        transform.position = y;
+    }
+}
